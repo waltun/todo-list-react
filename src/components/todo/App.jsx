ae22eb145422e8427e09c36886e26406efad29d6
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-05-22 15:23:51 +0430

    Get users from end api and show them

--- src/components/todo/App.jsx
+++ src/components/todo/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // import Components
 import Header from "../layout/header";
@@ -8,9 +8,19 @@ import TodoList from "./todoList";
 // import Context
 import TodosContext from "../../context/todosContext";
 
+// import Axios
+import axios from "axios";
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [done, setDone] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://6283d9436b6c317d5ba74d17.endapi.io/todos")
+      .then((response) => setTodos(response.data.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
